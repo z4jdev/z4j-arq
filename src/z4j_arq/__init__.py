f104ed3ff9fdacb2fadeaaab16f6a5dedf9d09eb
pypv <106410335+pypv@users.noreply.github.com>
2026-04-30 00:24:39 -0400

    Release 1.3.0

--- src/z4j_arq/__init__.py
+++ src/z4j_arq/__init__.py
@@ -5,6 +5,6 @@ from __future__ import annotations
 from z4j_arq.engine import ArqEngineAdapter
 from z4j_arq.events import attach_to_worker_settings
 
-__version__ = "1.0.0"
+__version__ = "1.3.0"
 
 __all__ = ["ArqEngineAdapter", "__version__", "attach_to_worker_settings"]
